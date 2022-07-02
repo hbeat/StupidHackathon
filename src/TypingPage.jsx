0d32606ac8b70762047d93e1df7afa414a2f2018
Fluke <48594334+Fkie01@users.noreply.github.com>
2022-07-02 16:14:48 +0700

    [update] typing page style

--- src/TypingPage.jsx
+++ src/TypingPage.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
+import "./typingpagestyle.css"
 
 const TypingPage = () => {
   return (
-    <>
+    <>    
+ 
       <h1>ด่าโลด</h1>
       <form>
          <textarea id="textarea" name="textarea" rows="20" cols="40"></textarea>
          <br/>
          <label>ขั้นต่ำ 100 คำนะกิ้วๆ</label>
          <br/>
-         <button>ล้างใหม่หมด</button>
-         <button>ด่าแม่งเลย</button>
+         <div>
+          <button className='button-2'>ล้างใหม่หมด</button>
+          <button className='button-1'>ด่าแม่งเลย</button>
+         </div>
+
+         
       </form>
+
+    
     </>
   )
 }
